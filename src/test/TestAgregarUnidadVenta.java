4bd168780cd1e69683d06dc9a9449efa900430b6
diego-olm <57022411+diego-olm@users.noreply.github.com>
2026-09-01 21:36:20 -0300

    agrego test actualizar traer y agregar

---
+++ src/test/TestAgregarUnidadVenta.java
@@ -0,0 +1,30 @@
+package test;
+
+
+
+
+import datos.UnidadVenta;
+import negocio.UnidadVentaABM;
+
+public class TestAgregarUnidadVenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UnidadVentaABM abm = new UnidadVentaABM();
+		
+		try {
+			abm.agregarUnidadVenta("Taco Movil", null, 25.5, "ABCDEFGHIJ", "ABC-123", true);
+  		    abm.agregarUnidadVenta("Burger Express",null, 18.0, "HJKLMWXYZQ", "XYZ-789", false);
+  		    abm.agregarUnidadVenta("Puesto de Comidas",null, 25.5, "AGRSHAEYFQ", 3, 120);
+  		    } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/test/TraerUnidadPorCodigo.java
@@ -0,0 +1,21 @@
+package test;
+
+import datos.UnidadVenta;
+import negocio.UnidadVentaABM;
+
+public class TraerUnidadPorCodigo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UnidadVentaABM abm = new UnidadVentaABM();
+		String codigo="ABCDEFGHIJ";
+//		String codigo2="HJKLMWXYZQ";
+		System.out.println("Treaer unidad por id: ABCDEFGHIJ ");
+		System.out.println(abm.traerUnidadVentaYEmpleados(codigo));
+	
+		System.out.println("---------------");
+		System.out.println("Traer todas las unidades de venta");
+		abm.traer().forEach(nombre -> System.out.println(nombre.toString()));
+	}
+
+}
