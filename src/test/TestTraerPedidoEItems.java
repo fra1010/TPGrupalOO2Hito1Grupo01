c47f0ace61acb1b51b55297797771b995ee4c38a
Leandro Vera <lean.ivan.vera@gmail.com>
2026-09-01 21:58:31 -0300

    Agrego Tests

---
+++ src/test/TestTraerPedidoEItems.java
@@ -0,0 +1,27 @@
+package test;
+
+import datos.ItemPedido;
+import datos.Pedido;
+import negocio.PedidoABM;
+
+public class TestTraerPedidoEItems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PedidoABM pedidoABM= new PedidoABM();
+		
+		Pedido pedido = pedidoABM.traerPedidoEItems(2);
+		
+		System.out.println("Pedido:");
+        System.out.println(pedido);
+        
+        System.out.println("Items:");
+
+        for (ItemPedido item : pedido.getItemsPedidos()) {
+            System.out.println(item);
+        }
+		
+	}
+
+}
