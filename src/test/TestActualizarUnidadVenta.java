a234d9cafd0cc255677134162955c7522a665903
diego-olm <57022411+diego-olm@users.noreply.github.com>
2026-09-16 11:49:39 -0300

    se actualizo el test actualizar unidad venta

--- src/test/TestActualizarUnidadVenta.java
+++ src/test/TestActualizarUnidadVenta.java
@@ -1,6 +1,8 @@
 package test;
 
+import datos.Festival;
 import datos.UnidadVenta;
+import negocio.FestivalABM;
 import negocio.UnidadVentaABM;
 
 public class TestActualizarUnidadVenta {
@@ -8,15 +10,19 @@ public class TestActualizarUnidadVenta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UnidadVentaABM abm = new UnidadVentaABM();
-		String codigo="ABCDEFGHIJ";
+		String codigo="ABCDEFGYIJ";
+		FestivalABM festivalAbm = new FestivalABM();
+		Festival festival = festivalAbm.traer(1);
 		UnidadVenta unidad= abm.traer(codigo);
 		System.out.println(unidad.toString());
 		System.out.println("--------------");
 		System.out.println("se actuliza unidad nombre de unidad de venta");
-		unidad.setNombre("Hamburguesa Express");
+		unidad.setFestival(festival);
 		abm.actualizar(unidad);
-		System.out.println(abm.traer(codigo).toString());
-		System.out.println();
+		UnidadVenta unidadVenta = abm.traer(codigo);
+		System.out.println(unidadVenta.toString());
+		System.out.println(unidadVenta.getFestival().toString());
+		
 	}
 
 }
