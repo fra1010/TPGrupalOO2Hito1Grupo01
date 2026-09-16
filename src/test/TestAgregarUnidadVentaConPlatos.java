952cf8c8444f77cc110feb12cc850290b6654c21
diego-olm <57022411+diego-olm@users.noreply.github.com>
2026-09-16 13:09:05 -0300

    actualizacion de test

--- src/test/TestAgregarUnidadVentaConPlatos.java
+++ src/test/TestAgregarUnidadVentaConPlatos.java
@@ -11,23 +11,23 @@ import negocio.UnidadVentaABM;
 public class TestAgregarUnidadVentaConPlatos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		UnidadVentaABM abm = new UnidadVentaABM();
-		FestivalABM festivalAbm= new FestivalABM();
-		Festival festival = festivalAbm.traer(1);
-		Set<Plato> listaDePlatos=new HashSet<>();
-		listaDePlatos.add(new Plato("pancho", 5000.00, 3000));
-		listaDePlatos.add(new Plato("Milanesa con papas", 8500.00, 4200.00));
-		listaDePlatos.add(new Plato("Ravioles con tuco", 7800.00, 3500.00));
-		listaDePlatos.add(new Plato("Lomo a la plancha", 12000.00, 5800.00));
-		listaDePlatos.add(new Plato("Pollo al horno con puré", 9000.00, 4300.00));
-		
-		try {
-			abm.agregarUnidadVentaFoodTruckConPlatos("Taco Movil", null, 25.5, "ABXDEFGHIJ",festival, "ABC-123", true,listaDePlatos);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		// TODO Auto-generated method stub
+//		UnidadVentaABM abm = new UnidadVentaABM();
+//		FestivalABM festivalAbm= new FestivalABM();
+//		Festival festival = festivalAbm.traer(1);
+//		Set<Plato> listaDePlatos=new HashSet<>();
+//		listaDePlatos.add(new Plato("pancho", 5000.00, 3000));
+//		listaDePlatos.add(new Plato("Milanesa con papas", 8500.00, 4200.00));
+//		listaDePlatos.add(new Plato("Ravioles con tuco", 7800.00, 3500.00));
+//		listaDePlatos.add(new Plato("Lomo a la plancha", 12000.00, 5800.00));
+//		listaDePlatos.add(new Plato("Pollo al horno con puré", 9000.00, 4300.00));
+//		
+//		try {
+//			abm.agregarUnidadVentaFoodTruckConPlatos("Taco Movil", null, 25.5, "ABXDEFGHIJ",festival, "ABC-123", true,listaDePlatos);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 
 	}
 
