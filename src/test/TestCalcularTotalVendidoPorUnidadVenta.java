8feb33a1a18a82800cad7d0578d4da45fa784af8
diego-olm <57022411+diego-olm@users.noreply.github.com>
2026-09-17 11:51:35 -0300

    se agrego TestCalcularTotalVendidoPorUnidadVenta

--- src/test/TestCalcularTotalVendidoPorUnidadVenta.java
+++ src/test/TestCalcularTotalVendidoPorUnidadVenta.java
@@ -16,45 +16,24 @@ public class TestCalcularTotalVendidoPorUnidadVenta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PlatoABM platoABM =  new PlatoABM();
-		PedidoABM pedidoABM = new PedidoABM();
-        ItemPedidoABM itemABM = new ItemPedidoABM();
+
+       
         UnidadVentaABM abm = new UnidadVentaABM();
-        FestivalABM festivalAbm= new FestivalABM();
-        Festival festival= festivalAbm.traer(1);
+        
         try {
-			abm.agregarUnidadVenta("Taco Movil", null, 25.5, "ABCDEFGHIJ",festival, "ABC-123", true);
-		       //SE TRAE LA UNIDAD DE VENTA POR CODIGO
-	        UnidadVenta unidad = abm.traer("ABCDEFGHIJ");
-			//AGERGAR PLATOS A LA UNIDAD DE VENTA
-			int idPlato1 = platoABM.agregar("Hamburguesa", 25000, 15000,unidad);
-			
-			int idPlato2 = platoABM.agregar("Pizza", 22000, 10000,unidad);
 
-			int idPlato3 = platoABM.agregar("Empanada", 3000, 1700,unidad);
-
-	        // AGREGAR PEDIDO
-	        int idPedido1 = pedidoABM.agregar(LocalDate.now(),unidad);
-	        int idPedido2 = pedidoABM.agregar(LocalDate.now(),unidad);
-
-	        // TRAER PARA RELACIONAR
-	        Plato plato1 = platoABM.traer(idPlato1);
-	        Pedido pedido1 = pedidoABM.traer(idPedido1);
-	        
-	        Plato plato2 = platoABM.traer(idPlato2);
-	        Plato plato3 = platoABM.traer(idPlato3);
-	        
-	        Pedido pedido2 = pedidoABM.traer(idPedido2);
-
-	        // AGREGAR ITEM
-	        itemABM.agregar(plato1, pedido1, 2);
-	        itemABM.agregar(plato3, pedido1, 4);
-
-	        itemABM.agregar(plato2, pedido2, 1);
-	        itemABM.agregar(plato3, pedido2, 6);
-	        // TOTAL VENDIDO POR UNIDAD DE VENTA
+	        UnidadVenta unidad = abm.traer("ABCDEFGYIJ");
+	        UnidadVenta unidad2 = abm.traer("HJKLBXYZQ");
+	        UnidadVenta unidad3 = abm.traer("AGISHAEYFQ");
+	        double total =abm.calcularTotalUnidadVenta(unidad.getCodigo());
+	        double total2 =abm.calcularTotalUnidadVenta(unidad2.getCodigo());
+	        double total3 =abm.calcularTotalUnidadVenta(unidad3.getCodigo());
 	        System.out.println("Unidad De Venta" + unidad.getNombre());
-	        System.out.println("Total vendido por unidad:  "+ abm.calcularTotalUnidadVenta(unidad.getCodigo()));
+	        System.out.println("Total vendido por unidad:  "+total );
+	        System.out.println("Unidad De Venta" + unidad2.getNombre());
+	        System.out.println("Total vendido por unidad:  "+ total2);
+	        System.out.println("Unidad De Venta" + unidad3.getNombre());
+	        System.out.println("Total vendido por unidad:  "+ total3);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
