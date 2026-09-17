f6db6fe3fdf8a6e53a35c52d3b90d1d84255e512
diego-olm <57022411+diego-olm@users.noreply.github.com>
2026-09-17 11:49:51 -0300

    se agrego TestCalcularGananciaPlatosPorUnidadVenta

---
+++ src/test/TestCalcularGananciaPlatosPorUnidadVenta.java
@@ -0,0 +1,23 @@
+package test;
+
+import negocio.UnidadVentaABM;
+
+public class TestCalcularGananciaPlatosPorUnidadVenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UnidadVentaABM abmUnidadVenta= new UnidadVentaABM();
+		String codigo = "ABCDEFGYIJ";
+		String codigo2 ="HJKLBXYZQ";
+		String codigo3 ="AGISHAEYFQ";
+
+		double ganancia = abmUnidadVenta.calcularGananciaPlatosPorUnidadVenta(codigo);
+		double ganancia2 = abmUnidadVenta.totalSueldoEmpleados(codigo2);
+		double ganancia3 = abmUnidadVenta.totalSueldoEmpleados(codigo3);
+		System.out.println("Ganancia de platos vendidos: " + ganancia  + " codigo unidad de venta: " + codigo);
+		System.out.println("Ganancia de platos vendidos: " + ganancia2 + " codigo unidad de venta: " + codigo2);
+		System.out.println("Ganancia de platos vendidos: " + ganancia3 + " codigo unidad de venta: " + codigo3);
+
+	}
+
+}
