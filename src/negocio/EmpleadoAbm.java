c77343a154c86928cf51bd7e23fa042eb4b015ca
ivanpablo1989 <ivaninfonet@gmail.com>
2026-09-24 15:49:52 -0300

    Ultima modificacion EmpleadoAbm.java

--- src/negocio/EmpleadoAbm.java
+++ src/negocio/EmpleadoAbm.java
@@ -1,3 +1,4 @@
+
 package negocio;
 
 import java.time.LocalDate;
@@ -5,207 +6,215 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dao.EmpleadoDao;
-import datos.Empleado;
-import datos.UnidadVenta;
-import datos.Cocinero;
 import datos.Cajero;
+import datos.Cocinero;
+import datos.Empleado;
 
-public class EmpleadoAbm 
+public class EmpleadoAbm
 {
-	private static EmpleadoAbm instancia = null;
+    private static EmpleadoAbm instancia = null;
 
-	public EmpleadoAbm() 
-	{
-		
-	}
+    public EmpleadoAbm()
+    {
+    }
 
-	public static EmpleadoAbm getInstance() 
-	{
-		if (instancia == null)
-			instancia = new EmpleadoAbm();
+    public static EmpleadoAbm getInstance()
+    {
+        if (instancia == null)
+        {
+            instancia = new EmpleadoAbm();
+        }
 
-		return instancia;
-	}
-	
-	public int agregarEmpleadoCocinero(String nombre,String apellido,long dni,LocalDate fechaNacimiento,
-			LocalDate ingreso,String especialidad,String categoria,int porcentaje) throws Exception
-	{
-		int id = 0;
+        return instancia;
+    }
 
-		if (EmpleadoDao.getInstance().traerPorDni(dni) != null)
-		{
-			throw new Exception("ERROR: ya existe un empleado con dicho DNI");
-		}
+    // -------------------------------------------------------------------
+    // Alta de empleados
+    // -------------------------------------------------------------------
 
-		id = EmpleadoDao.getInstance().agregar(new Cocinero(nombre,apellido,dni,fechaNacimiento,
-				ingreso,especialidad,categoria,porcentaje));
+    public int agregarEmpleadoCocinero(String nombre,String apellido,long dni,LocalDate fechaNacimiento,
+    		LocalDate ingreso,String especialidad,String categoria,int porcentaje) throws Exception
+    {
+        if (EmpleadoDao.getInstance().traerPorDni(dni) != null)
+        {
+            throw new Exception("ERROR: ya existe un empleado con dicho DNI");
+        }
 
-		return id;
-	}
-	
-	public int agregarEmpleadoCajero(String nombre,String apellido,long dni,LocalDate fechaNacimiento,
-			LocalDate ingreso,String turno,double plusAntiguedad) throws Exception
-	{
-		int id = 0;
+        return EmpleadoDao.getInstance().agregar(new Cocinero(nombre,apellido,dni,fechaNacimiento,
+                        ingreso,especialidad,categoria,porcentaje));
+    }
 
-		if (EmpleadoDao.getInstance().traerPorDni(dni) != null)
-		{
-			throw new Exception("ERROR: ya existe un empleado con dicho DNI");
-		}
+    public int agregarEmpleadoCajero(String nombre,String apellido,long dni,LocalDate fechaNacimiento,
+            LocalDate ingreso,String turno,double plusAntiguedad) throws Exception
+    {
+        if (EmpleadoDao.getInstance().traerPorDni(dni) != null)
+        {
+            throw new Exception("ERROR: ya existe un empleado con dicho DNI");
+        }
 
-		id = EmpleadoDao.getInstance().agregar(new Cajero(nombre,apellido,dni,fechaNacimiento,
-					ingreso,turno,plusAntiguedad));
+        return EmpleadoDao.getInstance().agregar(new Cajero(nombre,apellido,dni,fechaNacimiento,
+                        ingreso,turno,plusAntiguedad));
+    }
 
-		return id;
-	}
+    // -------------------------------------------------------------------
+    // Consultas basicas
+    // -------------------------------------------------------------------
 
-	public Empleado traer(int idEmpleado) throws Exception
-	{
-		Empleado e = EmpleadoDao.getInstance().traer(idEmpleado);
+    public Empleado traer(int idEmpleado) throws Exception
+    {
+        Empleado empleado = EmpleadoDao.getInstance().traerPorId(idEmpleado);
 
-		if (e == null)
-		{
-			throw new Exception("ERROR: no existe empleado con dicho ID");
-		}
-		return e;
-	}
+        if (empleado == null)
+        {
+            throw new Exception("ERROR: no existe empleado con dicho ID");
+        }
 
-	public List<Empleado> traer() throws Exception
-	{
-		List<Empleado> lista = EmpleadoDao.getInstance().traer();
+        return empleado;
+    }
 
-		if (lista.isEmpty())
-		{
-			throw new Exception("ERROR: no hay empleados registrados");
-		}
-		return lista;
-	}
+    public List<Empleado> traer() throws Exception
+    {
+        List<Empleado> empleados = EmpleadoDao.getInstance().traerTodos();
 
-	public void actualizar(Empleado empleado) 
-	{
-		EmpleadoDao.getInstance().actualizar(empleado);
-	}
-	
-    // ---- caso de uso 1: traer empleados por unidad de venta --------------------
-	
-	public List<Empleado> traerEmpleadosPorUnidad()
-	{
-	    return EmpleadoDao.getInstance().traerEmpleadosPorUnidadVenta();
-	}
-	
-	// ---- caso de uso 2: traer cantidades de empleados por unidad de venta -----------
-	
-	public List<String> traerCantidadEmpleados()
-	{
-	    return EmpleadoDao.getInstance().traerNumeroEmpleadosPorUnidadVenta();
-	}
+        if (empleados.isEmpty())
+        {
+            throw new Exception("ERROR: no hay empleados registrados");
+        }
 
-	// ---- caso de uso 3: traer empleados más antiguos --------------------
+        return empleados;
+    }
 
-	public List<Empleado> traerEmpleadosMasAntiguos(int top)
-	{
-	    return EmpleadoDao.getInstance().traerEmpleadosMasAntiguos(top);
-	}
+    public void actualizar(Empleado empleado)
+    {
+        EmpleadoDao.getInstance().actualizar(empleado);
+    }
 
-	// ---- caso de uso 4: traer aguinaldos --------------------
-	
-	public List<Object[]> traerAguinaldoPorEmpleado()
-	{
-	    List<Object[]> resultados = new ArrayList<Object[]>();
+    // -------------------------------------------------------------------
+    // Caso de uso 1: empleados por unidad
+    // -------------------------------------------------------------------
 
-	    List<Object[]> datos = EmpleadoDao.getInstance().traerDatosSueldo();
+    public List<Empleado> traerEmpleadosPorUnidad()
+    {
+        return EmpleadoDao.getInstance().traerEmpleadosPorUnidad();
+    }
 
-	    int anioActual = LocalDate.now().getYear();
+    // -------------------------------------------------------------------
+    // Caso de uso 2: cantidad de empleados por unidad
+    // -------------------------------------------------------------------
 
-	    for (Object[] dato : datos)
-	    {
-	        Empleado empleado = (Empleado) dato[0];
-	        String unidad = (String) dato[1];
-	        int sueldoBase = (Integer) dato[2];
+    public List<String> traerCantidadEmpleadosPorUnidad()
+    {
+        return EmpleadoDao.getInstance().traerCantidadEmpleadosPorUnidad();
+    }
 
-	        double adicional = 0;
+    // -------------------------------------------------------------------
+    // Caso de uso 3: empleados mas antiguos
+    // -------------------------------------------------------------------
 
-	        if (empleado instanceof Cajero)
-	        {
-	            Cajero cajero = (Cajero) empleado;
-	            int antiguedad = anioActual - empleado.getIngreso().getYear();
-	            adicional = cajero.getPlusAntiguedad() * antiguedad;
-	        }
-	        else if (empleado instanceof Cocinero)
-	        {
-	            Cocinero cocinero = (Cocinero) empleado;
-	            adicional = sueldoBase * cocinero.getPorcentaje() / 100.0;
-	        }
+    public List<Empleado> traerEmpleadosMasAntiguos(int cantidad)
+    {
+        return EmpleadoDao.getInstance().traerEmpleadosMasAntiguos(cantidad);
+    }
 
-	        double sueldo = sueldoBase + adicional;
-	        double aguinaldo = sueldo / 2;
+    // -------------------------------------------------------------------
+    // Caso de uso 4: aguinaldo por empleado
+    // -------------------------------------------------------------------
 
-	        resultados.add(new Object[] {empleado,unidad,sueldoBase,adicional,aguinaldo});
-	    }
+    public List<Object[]> traerAguinaldoPorEmpleado()
+    {
+        List<Object[]> resultados = new ArrayList<Object[]>();
 
-	    return resultados;
-	}
+        List<Object[]> datos = EmpleadoDao.getInstance().traerEmpleadosConSueldo();
 
-	// ---- caso de uso 5: traer jubilaciones --------------------
-	
-	public List<Object[]> traerJubilacionPorEmpleado()
-	{
-	    List<Object[]> resultados = new ArrayList<Object[]>();
+        int anioActual = LocalDate.now().getYear();
 
-	    List<Object[]> datos = EmpleadoDao.getInstance().traerDatosSueldo();
+        for (Object[] dato : datos)
+        {
+            Empleado empleado = (Empleado) dato[0];
+            String unidad = (String) dato[1];
+            int sueldoBase = (Integer) dato[2];
 
-	    int anioActual = LocalDate.now().getYear();
+            double adicional = calcularAdicional(empleado,sueldoBase,anioActual);
 
-	    for (Object[] dato : datos)
-	    {
-	        Empleado empleado = (Empleado) dato[0];
-	        String unidad = (String) dato[1];
-	        int sueldoBase = (Integer) dato[2];
+            double sueldo = sueldoBase + adicional;
+            double aguinaldo = sueldo / 2;
 
-	        double adicional = 0;
+            resultados.add(new Object[]{empleado,unidad,sueldoBase,adicional,aguinaldo});
+        }
 
-	        // Cajero: plus fijo por cada anio de antiguedad
-	        
-	        if (empleado instanceof Cajero)
-	        {
-	            Cajero cajero = (Cajero) empleado;
+        return resultados;
+    }
 
-	            int antiguedad = anioActual - empleado.getIngreso().getYear();
-	            adicional = cajero.getPlusAntiguedad() * antiguedad;
-	        }
+    // -------------------------------------------------------------------
+    // Caso de uso 5: aporte jubilatorio por empleado
+    // -------------------------------------------------------------------
 
-	        // Cocinero: porcentaje sobre el sueldo base
-	        
-	        else if (empleado instanceof Cocinero)
-	        {
-	            Cocinero cocinero = (Cocinero) empleado;
-	            adicional = sueldoBase * cocinero.getPorcentaje() / 100.0;
-	        }
+    public List<Object[]> traerJubilacionPorEmpleado()
+    {
+        List<Object[]> resultados = new ArrayList<Object[]>();
 
-	        double sueldo = sueldoBase + adicional;
+        List<Object[]> datos = EmpleadoDao.getInstance().traerEmpleadosConSueldo();
 
-	        // Aporte jubilatorio: 11%
-	        
-	        double jubilacion = sueldo * 0.11;
+        int anioActual = LocalDate.now().getYear();
 
-	        resultados.add(new Object[] {empleado,unidad,sueldoBase,adicional,sueldo,jubilacion});
-	    }
+        for (Object[] dato : datos)
+        {
+            Empleado empleado = (Empleado) dato[0];
+            String unidad = (String) dato[1];
+            int sueldoBase = (Integer) dato[2];
 
-	    return resultados;
-	}
-	
-	// ---- caso de uso 6: traer empleados ingresados entre dos fechas ----
-	
-	public List<Empleado> traerEmpleadosEntreFechas(LocalDate fechaDesde, LocalDate fechaHasta)
-	{
-	    return EmpleadoDao.getInstance().traerEmpleadosEntreFechas(fechaDesde, fechaHasta);
-	}
+            double adicional = calcularAdicional(empleado,sueldoBase,anioActual);
 
-	// ---- caso de uso 7: total de sueldo por unidad --------------------
+            double sueldo = sueldoBase + adicional;
 
-	public List<Object[]> traerTotalSueldoPorUnidad()
-	{
-	    return EmpleadoDao.getInstance().traerTotalSueldoPorUnidad();
-	}
-   
+            double jubilacion = sueldo * 0.11;
+
+            resultados.add(new Object[]{empleado,unidad,sueldoBase,adicional,sueldo,jubilacion});
+        }
+
+        return resultados;
+    }
+
+    // -------------------------------------------------------------------
+    // Calculo del adicional segun el tipo de empleado
+    // -------------------------------------------------------------------
+
+    private double calcularAdicional(Empleado empleado,int sueldoBase,int anioActual)
+    {
+        if (empleado instanceof Cajero)
+        {
+            Cajero cajero = (Cajero) empleado;
+
+            int antiguedad = anioActual - empleado.getIngreso().getYear();
+
+            return cajero.getPlusAntiguedad() * antiguedad;
+        }
+
+        if (empleado instanceof Cocinero)
+        {
+            Cocinero cocinero = (Cocinero) empleado;
+
+            return sueldoBase * cocinero.getPorcentaje() / 100.0;
+        }
+
+        return 0;
+    }
+
+    // -------------------------------------------------------------------
+    // Caso de uso 6: empleados ingresados entre dos fechas
+    // -------------------------------------------------------------------
+
+    public List<Empleado> traerEmpleadosEntreFechas(LocalDate fechaDesde,LocalDate fechaHasta)
+    {
+        return EmpleadoDao.getInstance().traerEmpleadosEntreFechas(fechaDesde,fechaHasta);
+    }
+
+    // -------------------------------------------------------------------
+    // Caso de uso 7: total de sueldo por unidad
+    // -------------------------------------------------------------------
+
+    public List<Object[]> traerTotalSueldosPorUnidad()
+    {
+        return EmpleadoDao.getInstance().traerTotalSueldosPorUnidad();
+    }
 }
